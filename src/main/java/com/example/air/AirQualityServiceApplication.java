b4caa5be63e801a6c4fa098581fb7c8cf02a0b39
KimJoongHyeok <moon960427@gmail.com>
2022-02-18 01:54:54 +0900

    Spring Cache 설정

--- src/main/java/com/example/air/AirQualityServiceApplication.java
+++ src/main/java/com/example/air/AirQualityServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.air;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class AirQualityServiceApplication {
 
 	public static void main(String[] args) {
